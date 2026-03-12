ba6b1c2c673ec74e14eedc8897ef09021c581c0d
mohammedkhamash <mohammedkhamash3@gmail.com>
2026-03-12 14:29:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("btn").onclick = function(){
+
+let message = document.getElementById("msg");
+
+message.innerText = "✔ JavaScript is working successfully!";
+
+message.style.color = "green";
+
+let button = document.getElementById("btn");
+
+button.innerText = "Clicked";
+
+button.style.backgroundColor = "#27ae60";
+
+};
